dedc975ce3b142ce59a70021f174a2cad0173ab6
Stephan Schmitz <info@eyecatchup.com>
2011-08-04 03:48:06 +0300

    Defined new constant BING_APP_ID for new child class SEOstats_Bing

--- src/class.seostats.config.php
+++ src/class.seostats.config.php
@@ -1,29 +1,38 @@
 <?php
-	/**
-	 *  PHP class SEOstats
-	 *
-	 *  @package	class.seostats
-	 *  @updated	2011/04/29
-	 *  @author		Stephan Schmitz <eyecatchup@gmail.com>
-	 *  @copyright	2010-present, Stephan Schmitz
-	 *  @license	GNU General Public License (GPL)
-	 *
-	 *  GLOBALS
-	 */
- 
-	define('GOOGLE_TLD', 
-		'com');
-	define('USE_PAGERANK_CHECKSUM_API', 
-		true);
+    /**
+     *  PHP class SEOstats
+     *
+     *  @package    class.seostats
+     *  @link       https://github.com/eyecatchup/SEOstats/
+     *  @updated    2011/08/04
+     *  @author     Stephan Schmitz <eyecatchup@gmail.com>
+     *  @copyright  2010-present, Stephan Schmitz
+     *  @license    GNU General Public License (GPL)
+     *
+     *  @filename   ./class.seostats.config.php
+     *  @desc       Defines some global used class constants.
+     *
+     *  @changelog
+     *  date        author              change(s)
+     *  2011/08/04  Stephan Schmitz     Defined BING_APP_ID constant.
+     */
 
-	/**
-	 *  Optional changes.
-	 */
-	define('YAHOO_APP_ID', 
-		'oVFwwjnV34GEEZTLB3K_WW1YC9_VysYbCYQ4szoXTAHZscrDvMazUpFR7TR0wchmlA--');
-	
-	define('SEOMOZ_ACCESS_ID', 
-		'member-881a55bcfb');
-	define('SEOMOZ_SECRET_KEY', 
-		'7145d56a1279285be92e6e4875bba8ee');
+    define('GOOGLE_TLD',
+        'com');
+    define('USE_PAGERANK_CHECKSUM_API',
+        true);
+
+    /**
+     *  Optional changes.
+     */
+    define('YAHOO_APP_ID',
+        'oVFwwjnV34GEEZTLB3K_WW1YC9_VysYbCYQ4szoXTAHZscrDvMazUpFR7TR0wchmlA--');
+
+    define('BING_APP_ID',
+        'FCB316D398F094C53B8C7AA8DA24B2D58AB520F8');
+
+    define('SEOMOZ_ACCESS_ID',
+        'member-881a55bcfb');
+    define('SEOMOZ_SECRET_KEY',
+        '7145d56a1279285be92e6e4875bba8ee');
 ?>
