c3f678755298e5623dda5d2cf4ff8c6b053768d7
Stephan Schmitz <info@eyecatchup.com>
2011-08-09 00:00:10 +0300

    Fixed comment blocks.

--- src/seostats.facebook.php
+++ src/seostats.facebook.php
@@ -1,40 +1,51 @@
 <?php
-	/**
-	 *  PHP class SEOstats
-	 *
-	 *  @class      SEOstats_Facebook
-	 *  @package	class.seostats
-	 *  @updated	2011/04/29
-	 *  @author		Florent Cima <florentcm@gmail.com>
-	 *  @copyright	2011-present, Florent Cima/Stephan Schmitz
-	 *  @license	GNU General Public License (GPL)
-	 */
+    /**
+     *  PHP class SEOstats
+     *
+     *  @class      SEOstats_Google
+     *  @package    class.seostats
+     *  @link       https://github.com/eyecatchup/SEOstats/
+     *  @updated    2011/08/08
+     *  @author     Florent Cima <florentcm@gmail.com>
+     *  @copyright  2010-present, Stephan Schmitz, Florent Cima
+     *  @license    GNU General Public License (GPL)
+     *
+     *  @filename   ./seostats.facebook.php
+     *  @desc       Child class of SEOstats, extending the main class
+     *              by methods for http://www.facebook.com
+     *
+     *  @changelog
+     *  date        author              method: change(s)
+     *  2011/08/08  Florent Cima        first commit
+     */
 
 class SEOstats_Facebook extends SEOstats {
 
-	/**
-	 * Returns the total amount of pages for a Domain indexed at Yahoo!
-	 *
-	 * @access		private
-	 * @link 		https://graph.facebook.com/
-	 * @return		integer 				Returns the total amount of Facebook Shares for a single page
-	 */		
-	public static function getFacebookShares($q)
-	{
-		$url = 'https://graph.facebook.com/?ids=';
-	
-	    // Parameters
-		$url .= urlencode($q);
-		
-		//Execution and result of Json in $str
-		$str  = SEOstats::cURL($url);
-		
-		//Decode Json object
-		$data = json_decode($str);
-		
-		//Return only number of facebook shares
-		$r = $data->$q->shares; 
-		return ($r != NULL) ? $r : intval('0');
-	}
+    /**
+     * Returns the total amount of Facebook Shares for a single page
+     *
+     * @access       public
+     * @link         https://graph.facebook.com/
+     * @param    q   string             The URL to check.
+     * @return       integer            Returns the total amount of Facebook
+     *                                  Shares for a single page.
+     */
+    public static function getFacebookShares($q)
+    {
+        $url = 'https://graph.facebook.com/?ids=';
+
+        // Parameters
+        $url .= urlencode($q);
+
+        //Execution and result of Json in $str
+        $str  = SEOstats::cURL($url);
+
+        //Decode Json object
+        $data = json_decode($str);
+
+        //Return only number of facebook shares
+        $r = $data->$q->shares;
+        return ($r != NULL) ? $r : intval('0');
+    }
 }
 ?>
