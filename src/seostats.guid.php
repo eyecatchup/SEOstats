2a211d1e6f80de56fbdd59314aa0b96e2c0577f7
Florent Cima <florentcm@gmail.com>
2011-08-09 15:47:39 -0400

    New empty file seostats.guid.php
    Allow Sylvain to add a new feature in this file.

---
+++ src/seostats.guid.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 *  PHP class SEOstats
+	 *
+	 *  @class      SEOstats_Yahoo
+	 *  @package	class.seostats
+	 *  @updated	2011/08/09
+	 *  @author		Florent Cima <florentcm@gmail.com>
+	 *  @copyright	2010-present, Stephan Schmitz
+	 *  @license	GNU General Public License (GPL)
+	 */
+
+class SEOstats_Guid extends SEOstats {
+
+	
+}
+?>
