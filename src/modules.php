18ab5c88a964237f63f7f48b671fdc2b96947d33
Florent Cima <florentcm@gmail.com>
2011-08-15 10:05:25 -0400

    Update of modules.php
    Add support for unique GUID

--- src/modules.php
+++ src/modules.php
@@ -1,31 +1,24 @@
 <?php
-    /**
-     *  PHP class SEOstats
-     *
-     *  @package    class.seostats
-     *  @link       https://github.com/eyecatchup/SEOstats/
-     *  @updated    2011/08/04
-     *  @author     Stephan Schmitz <eyecatchup@gmail.com>
-     *  @copyright  2010-present, Stephan Schmitz
-     *  @license    GNU General Public License (GPL)
-     *
-     *  @filename   ./modules.php
-     *  @desc       Includes all SEOstats child classes.
-     *
-     *  @changelog
-     *  date        author              change(s)
-     *  2011/08/04  Stephan Schmitz     added seostats.bing.php
-     *  2011/08/08  Stephan Schmitz     added seostats.facebook.php
-     *                                  and seostats.twitter.php
-     */
-
+	/**
+	 *  PHP class SEOstats
+	 *
+	 *  @package	class.seostats
+	 *  @updated	2011/04/29
+	 *  @author		Stephan Schmitz <eyecatchup@gmail.com>
+	 *  @copyright	2010-present, Stephan Schmitz
+	 *  @license	GNU General Public License (GPL)
+	 *
+	 *  GLOBALS
+	 */
+ 
 include_once('seostats.google.php');
 include_once('seostats.yahoo.php');
-include_once('seostats.bing.php');
 include_once('seostats.majesticseo.php');
 include_once('seostats.seomoz.php');
 include_once('seostats.alexa.php');
+include_once('seostats.exception.php');
+include_once('seostats.bing.php');
 include_once('seostats.facebook.php');
 include_once('seostats.twitter.php');
-include_once('seostats.exception.php');
+include_once('seostats.guid.php');
 ?>
