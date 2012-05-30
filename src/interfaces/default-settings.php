6ad1988f6be011d71ca2fedd6b0aca89cc837bd5
Stephan Schmitz <info@eyecatchup.com>
2012-05-30 08:36:34 +0200

    Updated interfaces.

--- src/interfaces/default-settings.php
+++ src/interfaces/default-settings.php
@@ -2,18 +2,18 @@
 /**
  *  Default User Settings
  *
- *  @package	SEOstats
- *  @author	    Stephan Schmitz <eyecatchup@gmail.com>
- *  @updated	2012/05/30
+ *  @package    SEOstats
+ *  @author     Stephan Schmitz <eyecatchup@gmail.com>
+ *  @updated    2012/05/30
  */
 
-	interface default_settings
-	{
-		const GOOGLE_TLD = 'com';
-		const ERROR_LOGS = '0';
-		const ERRLOG_DIR = 'log/';
-		const EXPORT_DIR = 'data/';
-	}
+    interface default_settings
+    {
+        const GOOGLE_TLD = 'com';
+        const ERROR_LOGS = '0';
+        const ERRLOG_DIR = 'log/';
+        const EXPORT_DIR = 'data/';
+    }
 
 /* End of file default-settings.php */
 /* Location: ./src/interfaces/default-settings.php */
--- src/interfaces/package.php
+++ src/interfaces/package.php
@@ -2,19 +2,19 @@
 /**
  *  SEOstats package Vars
  *
- *  @package	SEOstats
- *  @author	    Stephan Schmitz <eyecatchup@gmail.com>
- *  @updated	2012/05/30
+ *  @package    SEOstats
+ *  @author     Stephan Schmitz <eyecatchup@gmail.com>
+ *  @updated    2012/05/30
  */
 
-	interface package
-	{
-		const VERSION_CODE = '2.5.0';
-		const LICENSE_TYPE = 'MIT Licence';
-		const LICENSE_URL = 'http://eyecatchup.mit-license.org/';
-		const AUTHOR_NAME = 'Stephan Schmitz';
-		const AUTHOR_MAIL = 'eyecatchup@gmail.com';
-	}
+    interface package
+    {
+        const VERSION_CODE = '2.5.0';
+        const LICENSE_TYPE = 'MIT Licence';
+        const LICENSE_URL = 'http://eyecatchup.mit-license.org/';
+        const AUTHOR_NAME = 'Stephan Schmitz';
+        const AUTHOR_MAIL = 'eyecatchup@gmail.com';
+    }
 
 /* End of file package.php */
 /* Location: ./src/interfaces/package.php */
