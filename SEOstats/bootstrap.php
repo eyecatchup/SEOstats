8c258a16ef3d86c74e5a1cc296b68b0ebd2918d1
Stephan Schmitz <eyecatchup@gmail.com>
2013-08-14 18:45:10 +0200

    Fix issues #26: Ensure __DIR__ is defined.

--- SEOstats/bootstrap.php
+++ SEOstats/bootstrap.php
@@ -22,6 +22,10 @@ if (!ini_get('date.timezone') && function_exists('date_default_timezone_set')) {
  *  For increased reliability, resolve os-specific system path.
  */
 
+// Define the __DIR__ constant for PHP versions < 5.3.
+// Note: http://www.php.net/manual/en/language.constants.predefined.php#107614
+define('__DIR__', dirname(__FILE__));
+
 $base_path = __DIR__;
 if (realpath( $base_path ) !== false) {
     $base_path = realpath($base_path).'/';
