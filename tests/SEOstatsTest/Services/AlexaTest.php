e05f995aaad93507aef3c0e36d5ce75f252ff3b4
Clemens Sahs <clemens.sahs@slide-worker.org>
2014-05-09 14:21:11 +0200

    add Alexa blank test

---
+++ tests/SEOstatsTest/Services/AlexaTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace SEOstatsTes\Services;
+
+use SEOstats\Services\Alexa;
+
+class AlexaTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @var SEOstats
+     */
+    protected $SUT;
+
+    public function setUp()
+    {
+        $this->SUT = new Alexa();
+    }
+
+    public function testGetDailyRank()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetWeeklyRank()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetMonthlyRank()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetGlobalRank()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSetRankingKeys()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetCountryRank()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetBacklinkCount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPageLoadTime()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetTrafficGraph()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_GetXPath()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_GetAlexaPage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRetInt()
+    {
+        $this->markTestIncomplete();
+    }
+}
