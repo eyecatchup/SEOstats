5b11dcbc1cee72f8f58eccc94ab8ad44d0d208c9
Clemens Sahs <clemens.sahs@slide-worker.org>
2014-08-26 22:47:49 +0200

    fix coding standard: - spaces - braces - line length

--- SEOstats/Services/Sistrix.php
+++ SEOstats/Services/Sistrix.php
@@ -20,16 +20,16 @@ class Sistrix extends SEOstats
 {
     public static function getDBs()
     {
-      return array(
-        'de', //   de – Germany
-        'at', //   at – Austria
-        'ch', //   ch – Switzerland
-        'us', //   us – USA
-        'uk', //   uk – England
-        'es', //   es – Spain
-        'fr', //   fr – France
-        'it', //   it – Italy
-      );
+        return array(
+            'de', //   de – Germany
+            'at', //   at – Austria
+            'ch', //   ch – Switzerland
+            'us', //   us – USA
+            'uk', //   uk – England
+            'es', //   es – Spain
+            'fr', //   fr – France
+            'it', //   it – Italy
+        );
     }
 
     /**
@@ -62,108 +62,115 @@ class Sistrix extends SEOstats
      */
     public static function getVisibilityIndexByApi($url = false, $db = false)
     {
-      self::guardApiKey();
-      self::guardApiCredits();
+        self::guardApiKey();
+        self::guardApiCredits();
 
-      $url = parent::getUrl($url);
-      $domain = static::getDomainFromUrl($url);
-      $database = static::getValidDatabase($db);
+        $url = parent::getUrl($url);
+        $domain = static::getDomainFromUrl($url);
+        $database = static::getValidDatabase($db);
 
-      $dataUrl = sprintf(Config\Services::SISTRIX_API_VI_URL, Config\ApiKeys::SISTRIX_API_ACCESS_KEY, urlencode($domain), $database);
+        $dataUrl = sprintf(Config\Services::SISTRIX_API_VI_URL, Config\ApiKeys::SISTRIX_API_ACCESS_KEY, urlencode($domain), $database);
 
-      $json = static::_getPage($dataUrl);
+        $json = static::_getPage($dataUrl);
 
-      if(empty($json)) {
-        return parent::noDataDefaultValue();
-      }
-      
-      $json_decoded = (Helper\Json::decode($json, true));
-      if (!isset($json_decoded['answer'][0]['sichtbarkeitsindex'][0]['value'])) {
-        return parent::noDataDefaultValue();
-      }
-      return $json_decoded['answer'][0]['sichtbarkeitsindex'][0]['value'];
+        if(empty($json)) {
+            return parent::noDataDefaultValue();
+        }
+
+        $json_decoded = (Helper\Json::decode($json, true));
+        if (!isset($json_decoded['answer'][0]['sichtbarkeitsindex'][0]['value'])) {
+            return parent::noDataDefaultValue();
+        }
+        return $json_decoded['answer'][0]['sichtbarkeitsindex'][0]['value'];
     }
 
-    public static function getApiCredits() {
-      self::guardApiKey();
+    public static function getApiCredits()
+    {
+        self::guardApiKey();
 
-      $dataUrl = sprintf(Config\Services::SISTRIX_API_CREDITS_URL, Config\ApiKeys::SISTRIX_API_ACCESS_KEY);
-      $json = static::_getPage($dataUrl);
+        $dataUrl = sprintf(Config\Services::SISTRIX_API_CREDITS_URL, Config\ApiKeys::SISTRIX_API_ACCESS_KEY);
+        $json = static::_getPage($dataUrl);
 
-      if(empty($json) || $json == '{}') {
-        return parent::noDataDefaultValue();
-      }
-      $json_decoded = (Helper\Json::decode($json, true));
-      return $json_decoded['answer'][0]['credits'][0]['value'];
+        if(empty($json) || $json == '{}') {
+            return parent::noDataDefaultValue();
+        }
+        $json_decoded = (Helper\Json::decode($json, true));
+        return $json_decoded['answer'][0]['credits'][0]['value'];
     }
 
-    public static function checkApiCredits() {
-      return static::getApiCredits() > 0;
+    public static function checkApiCredits()
+    {
+        return static::getApiCredits() > 0;
     }
 
     protected static function guardApiKey()
     {
-      if(!static::hasApiKey()) {
-        self::exc('In order to use the SISTRIX API, you must obtain and set an
-          API key first (see SEOstats\Config\ApiKeys.php).'.PHP_EOL);
-      }
+        if(!static::hasApiKey()) {
+            self::exc('In order to use the SISTRIX API, you must obtain and set an ' .
+                      'API key first (see SEOstats\Config\ApiKeys.php).' . PHP_EOL);
+        }
     }
 
-    protected static function hasApiKey() {
-      if ('' == Config\ApiKeys::SISTRIX_API_ACCESS_KEY) {
-        return false;
-      }
-      return true;
+    protected static function hasApiKey()
+    {
+        if ('' == Config\ApiKeys::SISTRIX_API_ACCESS_KEY) {
+            return false;
+        }
+
+        return true;
     }
 
-    protected static function guardApiCredits() {
-      if(!static::checkApiCredits()) {
-        self::exc('Not enough API credits.'.PHP_EOL);
-      }
+    protected static function guardApiCredits()
+    {
+        if(!static::checkApiCredits()) {
+            self::exc('Not enough API credits.'.PHP_EOL);
+        }
     }
 
     private static function checkDatabase($db)
     {
-      return !in_array($db, self::getDBs()) ? false : $db;
+        return !in_array($db, self::getDBs()) ? false : $db;
     }
 
     protected static function getDomainFromUrl($url)
     {
-      $url      = parent::getUrl($url);
-      $domain   = Helper\Url::parseHost($url);
-      static::guardDomainIsValid($domain);
+        $url      = parent::getUrl($url);
+        $domain   = Helper\Url::parseHost($url);
+        static::guardDomainIsValid($domain);
 
-      return $domain;
+        return $domain;
     }
 
     protected static function getValidDatabase($db)
     {
-      $db = ($db == false) ? Config\DefaultSettings::SISTRIX_DB : $db;
+        $db = ($db == false) ? Config\DefaultSettings::SISTRIX_DB : $db;
 
-      $database = self::checkDatabase($db);
-      static::guardDatabaseIsValid($database);
+        $database = self::checkDatabase($db);
+        static::guardDatabaseIsValid($database);
 
-      return $database;
+        return $database;
     }
 
     protected static function guardDatabaseIsValid($database)
     {
-      if (false === $database) {
-        self::exc('db');
-      }
+        if (false === $database) {
+            self::exc('db');
+        }
     }
 
     protected static function guardDomainIsValid($domain)
     {
-      if (false == $domain) {
-        self::exc('Invalid domain name.');
-      }
+        if (false == $domain) {
+            self::exc('Invalid domain name.');
+        }
     }
 
     private static function exc($err)
     {
-      $e = ($err == 'db') ? "Invalid database. Choose one of: " .
-        substr( implode(", ", self::getDBs()), 0, -2) : $err;
-      throw new E($e);
+        $e = ($err == 'db')
+            ? "Invalid database. Choose one of: " .
+               substr( implode(", ", self::getDBs()), 0, -2)
+            : $err;
+        throw new E($e);
     }
 }
